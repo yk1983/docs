f05db66e95f109b9d440c1ec8a321b9d168c8f9a
yong kim <omiyong@gmail.com>
2024-07-23 15:47:34 +0900

    [240719][yong.kim] a태그 link태그 변환

--- frontend/src/layouts/Header.jsx
+++ frontend/src/layouts/Header.jsx
@@ -1,14 +1,15 @@
+import { Link } from 'react-router-dom';
+
 const Header = () => {
     return(
         <>
-            {/* ======= Header ======= */}
             <header id="header" className="header fixed-top d-flex align-items-center">
 
                 <div className="d-flex align-items-center justify-content-between">
-                    <a href="index.html" className="logo d-flex align-items-center">
+                    <Link to="index.html" className="logo d-flex align-items-center">
                         <img src={process.env.PUBLIC_URL + '/assets/img/logo.png'} alt="" />
-                        <span className="d-none d-lg-block">NiceAdmin</span>
-                    </a>
+                        <span className="d-none d-lg-block">DocsAdmin</span>
+                    </Link>
                     <i className="bi bi-list toggle-sidebar-btn"></i>
                 </div>{/* End Logo */}
 
@@ -24,24 +25,24 @@ const Header = () => {
                 <nav className="header-nav ms-auto">
                     <ul className="d-flex align-items-center">
                         <li className="nav-item d-block d-lg-none">
-                            <a className="nav-link nav-icon search-bar-toggle " href="#">
+                            <Link className="nav-link nav-icon search-bar-toggle " to="#">
                                 <i className="bi bi-search"></i>
-                            </a>
+                            </Link>
                         </li>{/* End Search Icon */}
 
                         <li className="nav-item dropdown">
 
-                            <a className="nav-link nav-icon" href="#" data-bs-toggle="dropdown">
+                            <Link className="nav-link nav-icon" to="#" data-bs-toggle="dropdown">
                                 <i className="bi bi-bell"></i>
                                 <span className="badge bg-primary badge-number">4</span>
-                            </a>{/* End Notification Icon */}
+                            </Link>{/* End Notification Icon */}
 
                             <ul className="dropdown-menu dropdown-menu-end dropdown-menu-arrow notifications">
                                 <li className="dropdown-header">
                                     You have 4 new notifications
-                                    <a href="#">
+                                    <Link to="#">
                                         <span className="badge rounded-pill bg-primary p-2 ms-2">View all</span>
-                                    </a>
+                                    </Link>
                                 </li>
 
                                 <li>
@@ -101,7 +102,7 @@ const Header = () => {
                                 </li>
 
                                 <li className="dropdown-footer">
-                                    <a href="#">Show all notifications</a>
+                                    <Link to="#">Show all notifications</Link>
                                 </li>
                             </ul>{/* End Notification Dropdown Items */}
 
@@ -109,17 +110,17 @@ const Header = () => {
 
                         <li className="nav-item dropdown">
 
-                            <a className="nav-link nav-icon" href="#" data-bs-toggle="dropdown">
+                            <Link className="nav-link nav-icon" to="#" data-bs-toggle="dropdown">
                                 <i className="bi bi-chat-left-text"></i>
                                 <span className="badge bg-success badge-number">3</span>
-                            </a>{/* End Messages Icon */}
+                            </Link>{/* End Messages Icon */}
 
                             <ul className="dropdown-menu dropdown-menu-end dropdown-menu-arrow messages">
                                 <li className="dropdown-header">
                                     You have 3 new messages
-                                    <a href="#">
+                                    <Link to="#">
                                         <span className="badge rounded-pill bg-primary p-2 ms-2">View all</span>
-                                    </a>
+                                    </Link>
                                 </li>
 
                                 <li>
@@ -127,14 +128,14 @@ const Header = () => {
                                 </li>
 
                                 <li className="message-item">
-                                    <a href="#">
+                                    <Link to="#">
                                         <img src={process.env.PUBLIC_URL + '/assets/img/messages-1.jpg'} alt="" className="rounded-circle" />
                                         <div>
                                             <h4>Maria Hudson</h4>
                                             <p>Velit asperiores et ducimus soluta repudiandae labore officia est ut...</p>
                                             <p>4 hrs. ago</p>
                                         </div>
-                                    </a>
+                                    </Link>
                                 </li>
 
                                 <li>
@@ -142,14 +143,14 @@ const Header = () => {
                                 </li>
 
                                 <li className="message-item">
-                                    <a href="#">
+                                    <Link to="#">
                                         <img src={process.env.PUBLIC_URL + '/assets/img/messages-2.jpg'} alt="" className="rounded-circle" />
                                         <div>
                                             <h4>Anna Nelson</h4>
                                             <p>Velit asperiores et ducimus soluta repudiandae labore officia est ut...</p>
                                             <p>6 hrs. ago</p>
                                         </div>
-                                    </a>
+                                    </Link>
                                 </li>
 
                                 <li>
@@ -157,14 +158,14 @@ const Header = () => {
                                 </li>
 
                                 <li className="message-item">
-                                    <a href="#">
+                                    <Link to="#">
                                         <img src={process.env.PUBLIC_URL + '/assets/img/messages-3.jpg'} alt="" className="rounded-circle" />
                                         <div>
                                             <h4>David Muldon</h4>
                                             <p>Velit asperiores et ducimus soluta repudiandae labore officia est ut...</p>
                                             <p>8 hrs. ago</p>
                                         </div>
-                                    </a>
+                                    </Link>
                                 </li>
 
                                 <li>
@@ -172,21 +173,21 @@ const Header = () => {
                                 </li>
 
                                 <li className="dropdown-footer">
-                                    <a href="#">Show all messages</a>
+                                    <Link to="#">Show all messages</Link>
                                 </li>
                             </ul>{/* End Messages Dropdown Items */}
                         </li>{/* End Messages Nav */}
 
                         <li className="nav-item dropdown pe-3">
-                            <a className="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
+                            <Link className="nav-link nav-profile d-flex align-items-center pe-0" to="#" data-bs-toggle="dropdown">
                                 <img src={process.env.PUBLIC_URL + '/assets/img/profile-img.jpg'} alt="Profile" className="rounded-circle" />
-                                <span className="d-none d-md-block dropdown-toggle ps-2">K. Anderson</span>
-                            </a>{/* End Profile Iamge Icon */}
+                                <span className="d-none d-md-block dropdown-toggle ps-2">Yong Kim</span>
+                            </Link>{/* End Profile Iamge Icon */}
 
                             <ul className="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
                                 <li className="dropdown-header">
-                                    <h6>Kevin Anderson</h6>
-                                    <span>Web Designer</span>
+                                    <h6>Yong Kim</h6>
+                                    <span>Svcs Del. Specialist</span>
                                 </li>
 
                                 <li>
@@ -194,10 +195,10 @@ const Header = () => {
                                 </li>
 
                                 <li>
-                                    <a className="dropdown-item d-flex align-items-center" href="users-profile.html">
+                                    <Link className="dropdown-item d-flex align-items-center" to="users-profile.html">
                                         <i className="bi bi-person"></i>
                                         <span>My Profile</span>
-                                    </a>
+                                    </Link>
                                 </li>
 
                                 <li>
@@ -205,10 +206,10 @@ const Header = () => {
                                 </li>
 
                                 <li>
-                                    <a className="dropdown-item d-flex align-items-center" href="users-profile.html">
+                                    <Link className="dropdown-item d-flex align-items-center" to="users-profile.html">
                                         <i className="bi bi-gear"></i>
                                         <span>Account Settings</span>
-                                    </a>
+                                    </Link>
                                 </li>
 
                                 <li>
@@ -216,10 +217,10 @@ const Header = () => {
                                 </li>
 
                                 <li>
-                                    <a className="dropdown-item d-flex align-items-center" href="pages-faq.html">
+                                    <Link className="dropdown-item d-flex align-items-center" to="pages-faq.html">
                                         <i className="bi bi-question-circle"></i>
                                         <span>Need Help?</span>
-                                    </a>
+                                    </Link>
                                 </li>
 
                                 <li>
@@ -227,10 +228,10 @@ const Header = () => {
                                 </li>
 
                                 <li>
-                                    <a className="dropdown-item d-flex align-items-center" href="#">
+                                    <Link className="dropdown-item d-flex align-items-center" to="#">
                                         <i className="bi bi-box-arrow-right"></i>
                                         <span>Sign Out</span>
-                                    </a>
+                                    </Link>
                                 </li>
                             </ul>{/* End Profile Dropdown Items */}
                         </li>{/* End Profile Nav */}

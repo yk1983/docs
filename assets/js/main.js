b683bc2b71fb7c5b94a4c0731567a63c0f2a902b
yong kim <omiyong@gmail.com>
2024-07-26 17:53:04 +0900

    Updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -42,20 +42,20 @@
     /**
      * Sidebar toggle
      */
-    if (select('.toggle-sidebar-btn')) {
-        on('click', '.toggle-sidebar-btn', function (e) {
-            select('body').classList.toggle('toggle-sidebar')
-        })
-    }
+//    if (select('.toggle-sidebar-btn')) {
+//        on('click', '.toggle-sidebar-btn', function (e) {
+//            select('body').classList.toggle('toggle-sidebar')
+//        })
+//    }
 
     /**
      * Search bar toggle
      */
-    if (select('.search-bar-toggle')) {
-        on('click', '.search-bar-toggle', function (e) {
-            select('.search-bar').classList.toggle('search-bar-show')
-        })
-    }
+//    if (select('.search-bar-toggle')) {
+//        on('click', '.search-bar-toggle', function (e) {
+//            select('.search-bar').classList.toggle('search-bar-show')
+//        })
+//    }
 
     /**
      * Navbar links active state on scroll
